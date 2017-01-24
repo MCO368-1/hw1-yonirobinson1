290c73d549d71927cc179db87811a7ae632eaa46
profyoni <jrobinson@touro.edu>
2017-01-23 22:27:35 -0500

    more test, fixed yml

--- Travis1CSharp.Test/UnitTest1.cs
+++ Travis1CSharp.Test/UnitTest1.cs
@@ -6,9 +6,24 @@ namespace Travis1CSharp.Test
     public class UnitTest1
     {
         [Test]
+        public void TestMethod0()
+        {
+            Assert.IsTrue(Travis1CSharp.Program.True());
+        }
+        [Test]
         public void TestMethod1()
         {
-            Assert.IsTrue(Travis1CSharp.Program.True()); 
+            Assert.IsTrue(Travis1CSharp.Program.True());
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(Travis1CSharp.Program.True());
+        }
+        [Test]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(!Travis1CSharp.Program.True());
         }
     }
 }
