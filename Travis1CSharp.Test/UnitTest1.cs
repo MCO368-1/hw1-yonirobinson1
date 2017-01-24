28b5ccc8f259a8506156c741bfd4e85f03c1ed25
profyoni <jrobinson@touro.edu>
2017-01-23 20:29:14 -0500

    1

---
+++ Travis1CSharp.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Travis1CSharp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Travis1CSharp.Program.True());
+        }
+    }
+}
